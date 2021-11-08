8f55d0efc3282122b9382c6216fbe046efefe326
HigorDenomar <higordenomar@hotmail.com>
2021-11-07 22:23:20 -0300

    header created

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+
+import { SignInButton } from '../SignInButton';
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <Image
+          src="/images/logo.svg"
+          alt="ig.news"
+          width={110}
+          height={40}
+        />
+
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
